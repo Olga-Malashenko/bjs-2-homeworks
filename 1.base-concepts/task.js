428799c39f2ad69049fc993972a1c02b5549d107
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-16 23:15:22 +0300

    The second task is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -22,32 +22,26 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
   //function calculateTotalMortgage(percent, contribution, amount, date) {
-    let totalAmount;
+    //let totalAmount;
   
     if (isNaN(+percent)) {
-      return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+      return false;
     }
     if (isNaN(+contribution)) {
-      return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+      return false;
     }
     if (isNaN(+amount)) {
-      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+      return false;
     }
   
+    const percentPerMonth = parseFloat(percent) / 100 / 12;
   
-    let percentPerMonth = parseFloat(percent) / 100 / 12;
+    const loan = parseFloat(amount) - parseFloat(contribution);
   
-    let loan = parseFloat(amount) - parseFloat(contribution);
-  
-    let currentDate = new Date();
-    let quantityOfYear = date.getFullYear() - currentDate.getFullYear();
-    let quantityOfMonth = (date.getMonth() - currentDate.getMonth()) + quantityOfYear * 12;
-  
-    let payment = loan * (percentPerMonth + percentPerMonth / (Math.pow((1 + percentPerMonth), quantityOfMonth) - 1));
-    totalAmount = Number((payment * quantityOfMonth).toFixed(2));
+    const payment = loan * (percentPerMonth + percentPerMonth / (Math.pow((1 + percentPerMonth), countMonths) - 1));
+    const totalAmount = Number((payment * countMonths).toFixed(2));
     
     console.log(`Сумма, которую клиент заплатит банку: ${totalAmount}\nВ том числе:\n\tПервоначальный взнос ${contribution}\n\tПогашение основного долга ${loan}\n\tПроценты за пользование кредитом ${(payment * quantityOfMonth - loan).toFixed(2)}`);
   
     return totalAmount;
   }
-}
