96a49e64672faec22bde50867a1e83bcb8107e8c
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-17 00:49:51 +0300

    Исправлена ошибка с rest оператором

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,10 +2,10 @@
 
 function getArrayParams(...arr) {
   
-    const min = Math.min(...arr);
-    const max = Math.max(...arr);
-    const sum = arr.reduce((acc, item) => acc + item, 0);
-    const avg = Number((sum / arr.length).toFixed(2));
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    let sum = arr.reduce((acc, item) => acc + item, 0);
+    let avg = Number((sum / arr.length).toFixed(2));
   
     return { min, max, avg };
 }
@@ -24,8 +24,8 @@ function differenceMaxMinWorker(...arr) {
   if (arr.length === 0) {
     return 0;
   }
-  const max = Math.max(...arr);
-  const min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
   return max - min;
 }
 
@@ -33,9 +33,9 @@ function differenceEvenOddWorker(...arr) {
   if (arr.length === 0) {
     return 0;
   }
-  const arrOfEven = arr.filter(number => number % 2 === 0);
-  const sumOfEven = arrOfEven.reduce((acc, number) => acc + number, 0);
-  const sumOfOdd = arr.filter(number => number % 2 === 1).reduce((acc, number) => acc + number, 0);
+  let arrOfEven = arr.filter(number => number % 2 === 0);
+  let sumOfEven = arrOfEven.reduce((acc, number) => acc + number, 0);
+  let sumOfOdd = arr.filter(number => number % 2 === 1).reduce((acc, number) => acc + number, 0);
   return sumOfEven - sumOfOdd;
 }
 
@@ -43,7 +43,7 @@ function averageEvenElementsWorker(...arr) {
   if (arr.length === 0) {
     return 0;
   }
-  const arrOfEven = arr.filter(number => number % 2 === 0);
+  let arrOfEven = arr.filter(number => number % 2 === 0);
   return arrOfEven.reduce((acc, element) => acc + element, 0) / arrOfEven.length;
 }
 
@@ -53,7 +53,7 @@ function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   
   for (let element of arrOfArr) {
-    let comparable = func(element)
+    let comparable = func(...element)
     if (comparable > maxWorkerResult) {
       maxWorkerResult = comparable;
     }
