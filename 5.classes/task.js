e1b2e10159ec32096f045c46035a45dd650137fb
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-21 21:19:06 +0300

    Вторая задача сделана

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,3 +62,39 @@ class DetectiveBook extends Book {
         this.type = "detective";
     }
 }
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            for (let property in book) {
+                if (book[property] == value && String(property) === String(type)) {
+                    return book;
+                } else {
+                    return null;
+                }
+            }
+        }  
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                let book = this.books[i];
+                this.books.splice(i, 1);
+                return book;
+            }
+        }
+    }
+
+}
