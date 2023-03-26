86ada406af61238e3b42380393f32138c176ce7b
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-26 18:31:52 +0300

    Первая задача сделана

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,15 @@
-﻿
+﻿function parseCount(quantity) {
+    let result = parseFloat(quantity);
+    if (isNaN(result)){
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(quantity) {
+    try {
+        parseCount(quantity);
+    } catch (error){
+        console.log(error);
+    }
+}
