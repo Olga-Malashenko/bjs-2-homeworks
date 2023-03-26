33278793fb58b479c70e4657d842e1295cd11ce9
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-26 22:58:53 +0300

    Вторая задача сделана

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,4 +1,6 @@
-﻿function parseCount(quantity) {
+﻿//  Задача 1
+
+function parseCount(quantity) {
     let result = Number.parseFloat(quantity);
     if (Number.isNaN(result)){
         throw new Error("Невалидное значение");
@@ -8,8 +10,43 @@
 
 function validateCount(quantity) {
     try {
-        parseCount(quantity);
+        return parseCount(quantity);
     } catch (error){
         return error;
     }
+}
+
+//  Задача 2
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        if (!(sideA > 0 && sideB > 0 && sideC > 0 && (sideA + sideB) > sideC && (sideA + sideC) > sideB && (sideB + sideC) > sideA)) {
+            throw new Error ("Треугольник ст такими сторонами не существует");
+        }
+
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        //this.perimeter = sideA + sideB + sideC;
+    }
+
+    getPerimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    getArea() {
+        let semiPerimeter = this.getPerimeter() * 0.5;
+        return Number(Math.sqrt(semiPerimeter * (semiPerimeter - this.sideA) * (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC)).toFixed(3));
+    }
+    
+}
+
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch(error) {
+        return error;
+    }
+    
 }
