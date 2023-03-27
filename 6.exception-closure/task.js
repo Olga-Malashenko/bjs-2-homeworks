049364b5a596b2e637507c75b287f0208b98159a
Olga Malashenko <olga_birjukova@mail.ru>
2023-03-27 19:10:41 +0300

    Методы заменены на геттеры

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -20,33 +20,34 @@ function validateCount(quantity) {
 
 class Triangle {
     constructor(sideA, sideB, sideC) {
-        if (!(sideA > 0 && sideB > 0 && sideC > 0 && (sideA + sideB) > sideC && (sideA + sideC) > sideB && (sideB + sideC) > sideA)) {
-            throw new Error ("Треугольник ст такими сторонами не существует");
+        if (!(sideA > 0 && sideB > 0 && sideC > 0 && (sideA + sideB) > sideC 
+        && (sideA + sideC) > sideB && (sideB + sideC) > sideA)) {
+            throw new Error ("Треугольник с такими сторонами не существует");
         }
-
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
-        //this.perimeter = sideA + sideB + sideC;
     }
 
-    getPerimeter() {
+    get perimeter() {
         return this.sideA + this.sideB + this.sideC;
     }
 
-    getArea() {
-        let semiPerimeter = this.getPerimeter() * 0.5;
-        return Number(Math.sqrt(semiPerimeter * (semiPerimeter - this.sideA) * (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC)).toFixed(3));
-    }
-    
+    get area() {
+        const semiPerimeter = this.perimeter * 0.5;
+        return Number(Math.sqrt(semiPerimeter * (semiPerimeter - this.sideA) * 
+        (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC)).toFixed(3));
+    }  
 }
-
 
 function getTriangle(sideA, sideB, sideC) {
     try {
         return new Triangle(sideA, sideB, sideC);
     } catch(error) {
-        return error;
+        return {
+            perimeter: "Ошибка! Треугольник не существует",
+            area : "Ошибка! Треугольник не существует"
+        };
     }
     
 }
