370cf14030c1c7f3f4a16bf2542cb6be79864391
Olga Malashenko <olga_birjukova@mail.ru>
2023-04-11 00:29:59 +0300

    Задание сделано

---
+++ 7.async/task.js
@@ -0,0 +1,60 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId;
+    }
+
+    addClock(formattedTime, func) {
+        
+        if(!this.time || !this.callback) {        // проверка, есть ли параметры (выбросить ошибку, если нет)
+            throw new Error('Отсутствуют обязательные аргументы');
+            return;
+        }
+
+        if(this.alarmCollection.find(sound => sound.time === formattedTime)) {    // проверка времени на повтор
+            console.warn('Уже присутствует звоок на это же время');
+            return; 
+        }
+        
+        const sound = {        // добавляем в массив объект (новый звонок в коллекцию)
+            callback: func,
+            time: formattedTime,
+            canCall: true
+        }
+        this.alarmCollection.push(sound);
+    }
+
+    removeClock(time) {
+        return this.alarmCollection.filter(sound => sound.time !== time);    //удаляет звонок по свойству time
+    }
+
+    getCurrentFormattedTime() {
+        const currentDate = new Date();
+        return currentDate.getHours() + ":" + currentDate.getMinutes();
+    }
+
+    start() {
+        if(this.intervalId) return;   // завершаем, если есть значение id
+        this.intervalId = setInterval(() => {
+            if(this.alarmCollection.forEach(sound => sound.time === getCurrentFormattedTime())) {
+                this.canCall = false;
+                this.callback();
+            }
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(sound => sound.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        alarmCollection = [];
+    }
+
+}
